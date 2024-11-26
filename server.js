f1b61bb01a67b7444e34827b189e2dd5430f583b
BassemBG <bgbassem8@gmail.com>
2024-11-26 15:39:08 +0100

    added DB config + conencted sequelize to DB

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "twig");
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.set("views", path.join(__dirname, "views")); //to say that oue template pages (html) are in a folder called views
+app.set("view engine", "twig"); // templates will be of twig format
+app.use(express.json()); // so that the project understands how to parse json
+app.use(express.urlencoded({ extended: false })); // how to parse url
 
 server.listen(3000, () => {
   console.log("server started on http://localhost:3000");
